2fb21605d6dcd1dad837a7d0d7f7b3e05afc6358
liam0504 <lhsu16@asu.edu>
2022-10-27 13:14:50 -0700

    First

---
+++ src/main/java/org/example/urinals.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.testng.annotations.Test;
+/**
+ * @author Li Che Hsu
+ * ASUrite: lhsu16
+ * ASU ID: 1225397459
+*/
+public class urinals {
+    public static void main(String[] args) {
+
+    }
+    Boolean goodString (String str) {  // checks to see if valid string
+        System.out.println ("Not yet implemented");
+        return true;
+    }
+}
---
+++ src/main/java/org/example/urinalsTest.java
@@ -0,0 +1,9 @@
+package org.example;
+
+/**
+ * @author Li Che Hsu
+ * ASUrite: lhsu16
+ * ASU ID: 1225397459
+ */
+public class urinalsTest {
+}
